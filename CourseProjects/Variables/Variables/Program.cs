284b901c6ee6de2399a0f008b1f2afa1a44ca2aa
Tracy Hartzler <thartzler@gmail.com>
2014-12-24 16:49:03 -0800

    new project for variables code

---
+++ CourseProjects/Variables/Variables/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            */
+
+            //string myFirstName;
+            //myFirstName = "Bob";
+            //string myFirstName = "Bob";
+
+            var myFirstName = "Bob";
+
+            Console.WriteLine(myFirstName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
