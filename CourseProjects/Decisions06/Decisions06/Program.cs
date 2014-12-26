e9ff569cd8a0c13d9db1966a010ea7358bc0cf84
Tracy Hartzler <thartzler@gmail.com>
2014-12-26 12:19:11 -0800

    commented out user input code - userValue in lesson 6

---
+++ CourseProjects/Decisions06/Decisions06/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please type something and press the Enter key.");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed: " + userValue);
+            //Console.ReadLine();
+
+        }
+    }
+}
