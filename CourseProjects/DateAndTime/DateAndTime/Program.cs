50f4429b89e735d1a25d7f424cc5b4328b7d0a8a
Tracy Hartzler <thartzler@gmail.com>
2015-01-02 15:35:37 -0800

    finished lesson 14 Date and Time

--- CourseProjects/DateAndTime/DateAndTime/Program.cs
+++ CourseProjects/DateAndTime/DateAndTime/Program.cs
@@ -10,6 +10,31 @@ namespace DateAndTime
     {
         static void Main(string[] args)
         {
+            DateTime myValue = DateTime.Now;
+           // Console.WriteLine(myValue.ToString());
+           // Console.WriteLine(myValue.ToShortDateString());
+           // Console.WriteLine(myValue.ToShortTimeString()); 
+           // Console.WriteLine(myValue.ToLongDateString());
+           // Console.WriteLine(myValue.ToLongTimeString()); 
+           
+           // Console.WriteLine(myValue.AddDays(3).ToLongDateString()); 
+           // Console.WriteLine(myValue.AddHours(3).ToLongTimeString()); 
+
+           // Console.WriteLine(myValue.AddDays(-3).ToShortDateString());
+           // Console.WriteLine(myValue.Month.ToString());
+
+         //via overloaded constructor
+            //DateTime myBirthday = new DateTime(1956, 10, 30);
+            //Console.WriteLine(myBirthday.ToShortDateString());
+
+
+        //subtract method to determine the amount of time between when I was born to now
+            DateTime myBirthday = DateTime.Parse("10/30/1956");
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+
         }
     }
 }
