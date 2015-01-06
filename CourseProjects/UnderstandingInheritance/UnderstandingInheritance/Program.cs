e3d5a5394593cc4b74b5ac847f63946277d9c5be
Tracy Hartzler <thartzler@gmail.com>
2015-01-06 10:34:59 -0800

    lesson 17 example of Abstract class with comments. Code version 2

--- CourseProjects/UnderstandingInheritance/UnderstandingInheritance/Program.cs
+++ CourseProjects/UnderstandingInheritance/UnderstandingInheritance/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Example of original individual classes 'Car' and 'Truck' being reworked into a new abstract class 'Vehicle'.
+
+
 namespace UnderstandingInheritance
 {
     class Program
@@ -16,9 +20,11 @@ namespace UnderstandingInheritance
             myCar.Color = "Black";
             myCar.Year = 2005;
 
-            printCarDetails(myCar);
+           // printCarDetails(myCar);
+            printVehicleDetails(myCar);
 
             // instantiate a new instance of Truck (this was done after creating the Truck class in line 54
+           
             Truck myTruck = new Truck();
 
             myTruck.Make = "Ford";
@@ -26,29 +32,58 @@ namespace UnderstandingInheritance
             myTruck.Year = 2006;
             myTruck.Color = "Black";
             myTruck.TowingCapacity = 1200;
-            printCarDetails(myTruck);     //anywhere 'Car' would work, 'Truck' would work as well since 'Truck' is a "type" of 'Car' but has an extra property
+      //printCarDetails(myTruck);     
+            
+            //anywhere 'Car' would work, 'Truck' would work as well since 'Truck' is a "type" of 'Car' but has an extra property
             //parent/child class or Superclass etc.
+
+            // 'Truck' is no longer a type of 'Car' it is a type of new class 'Vehicle' so the above line creates an error
+            // has changed from parent/child to sibling of 'Car'
+            // has changed from 'Car' to 'Vehicle' also
+
+            printVehicleDetails(myTruck);
 
             Console.ReadLine();
 
         }
 
         //helper method that belongs to the Program class
-        private static void printCarDetails(Car car)
+       
+        //private static void printCarDetails(Car car)  // not true anymore since adding 'Vehicle' class, not car details but vehicle details now
+        
+        private static void printVehicleDetails(Vehicle vehicle)  
+
         {
-            Console.WriteLine("Here are the Car's details: {0}",
-                car.FormatMe());
+            //Console.WriteLine("Here are the Car's details: {0}",
+                // car.FormatMe());
+            
+            Console.WriteLine("Here are the Vehicle's details: {0}",    
+            vehicle.FormatMe());
         }
     }
 
-    class Car
+    //new abstract class
+    abstract class Vehicle
     {
         public string Make { get; set; }
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+       public abstract string FormatMe(); //can't create an instance of abstract FormatMe(), so you have to have an instance of FormatMe() whenever you derive from 'Vehicle'
+    }
+
+    //class Car
+    class Car : Vehicle
+    {
+//        public string Make { get; set; }
+//        public string Model { get; set; }
+//        public int Year { get; set; }
+//        public string Color { get; set; }
        
-        public virtual string FormatMe()
+      //  public abstract string FormatMe()  //create an abstract class above to work with both cars and trucks at a higher level
+        //now that we have the abstract class Vehicle, this needs to use 'overide'
+        public override string FormatMe()
         {
             return String.Format("{0} - {1} - {2} - {3}",
                 this.Make,
@@ -59,12 +94,15 @@ namespace UnderstandingInheritance
        
      }
 
-    // create a new class 'Truck'
-    class Truck : Car  // the ':' means inherite everything from the 'Car' class and add the following property
+    // create a new class 'Truck' from 'Car'
+    // class Truck : Car  // the ':' means inherit everything from the 'Car' class and add the following property
+    // modifying class Truck to derive from new abstract class Vehicle
+
+    class Truck : Vehicle
     {
         public int TowingCapacity { get; set; }
         
-        //creating an override method so FormatMe() works with trucks, added 'virtual' to the original FormatMe() method above 
+        //creating an override method so FormatMe() works with trucks 
         public override string FormatMe()
         {
             return String.Format("{0} - {1} - {2} Towing units",
