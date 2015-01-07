8b08fad6bf9c87dc2e3624eae0fddaf06737e6d9
Tracy Hartzler <thartzler@gmail.com>
2015-01-07 11:46:40 -0800

    Final lesson 18 code

--- CourseProjects/NamespacessAndReferencingAssemblies/NamespacessAndReferencingAssemblies/Program.cs
+++ CourseProjects/NamespacessAndReferencingAssemblies/NamespacessAndReferencingAssemblies/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.IO;    //added by option menu for 'StreamReader below
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tabor;
 
 namespace NamespacessAndReferencingAssemblies
 {
@@ -17,7 +18,19 @@ namespace NamespacessAndReferencingAssemblies
 
            // use the Control key and the period to open up the option menu instead of trying to hover over the little box underneath StreamReader
             
-            StreamReader myStreamReader = new StreamReader();
+           // StreamReader myStreamReader = new StreamReader();
+
+  // added the Lesson 18 sample 'Tabor.dll' file to References which contains "Bob" class
+            //added 'using Tabor' to the using list above
+
+            Bob bob = new Bob();
+
+            string html = bob.Lookup("http://www.learnvisualstudio.net");
+
+            Console.WriteLine(html);
+            Console.ReadLine();
+            // opened the console with a lot of html from Tabor's website
+
 
 
         }
