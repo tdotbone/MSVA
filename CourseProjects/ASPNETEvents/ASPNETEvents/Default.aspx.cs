22cbec1b2850b76330d5c75ed4d97dcb49f76078
Tracy Hartzler <thartzler@gmail.com>
2015-01-09 11:23:30 -0800

    Lesson 24 final code

--- CourseProjects/ASPNETEvents/ASPNETEvents/Default.aspx.cs
+++ CourseProjects/ASPNETEvents/ASPNETEvents/Default.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//MSVA Course C# Fundamentals for Absolute Beginners, Lesson 24 Understanding Event Driven Programming
 namespace ASPNETEvents
 {
     public partial class Default : System.Web.UI.Page
